4c95c92f6edc611f33f431fd9692ce5b281b7afb
Mares Popa <popa.mares@gmail.com>
2022-12-15 13:35:05 +0200

    feat(5) - update keywords

--- app/components/landing/Landing.tsx
+++ app/components/landing/Landing.tsx
@@ -6,19 +6,22 @@ const Landing = () => {
       <div className="text-center text-gray-800 py-20 px-6">
         <h1 className="text-5xl font-bold mt-0 mb-6">Welcome!</h1>
 
-        <p className="my-10 mx-auto text-3xl">
-          {`Hi, I'm Mares! I'm a fronted engineer with more than half a decade of
+        <p className="my-10 mx-auto text-xl">
+          {`Hi, I'm Mares! I'm a frontend engineer with more than half a decade of
           experience in developing web applications that help organizations
           address business challenges and meet their needs. My expertise lies
-          within front-end web apps, and the main keywords in my tech stack are
-          JavaScript, Typescript, Next.JS, React, and of course HTML/CSS. `}
+          within front-end web apps, and the main keywords in my tech stack are `}
+          <span className="font-bold">
+            {'JavaScript, Typescript, Next.JS, React, '}
+          </span>
+          {'and of course '} <span className="font-bold">HTML/CSS.</span>
         </p>
-        <p className="my-4 mx-auto text-3xl">
+        <p className="my-4 mx-auto text-md">
           {`I live in Romania, married to a wonderful wife and been alive since
           93'. Beside being obsessed with the web, I also enjoy cycling,
           basketball and visiting new cities.`}
         </p>
-        <p className="my-4 mx-auto text-3xl">
+        <p className="my-4 mx-auto text-md">
           <br />
           {`Have an interesting proposal? Contact me below and I'll get back to you as soon as I can.`}
         </p>
