b2929bf8d834468228034fd3ff3d9dd4feed252a
smoothwizz <a.popa@goit.global>
2024-01-20 15:24:42 +0200

    feat: update navigation colors

--- app/components/common/global-navigation/GlobalNavigation.tsx
+++ app/components/common/global-navigation/GlobalNavigation.tsx
@@ -12,7 +12,7 @@ const GlobalNavigation = () => {
   const hamburgerOpen = <Icon type="hamburger" size={18} />
 
   return (
-    <header className="bg-sky-800 md:bg-sky-950">
+    <header className="bg-teal-900 md:bg-teal-900">
       <nav className="w-full">
         <Container>
           <div className="justify-between mx-auto lg:max-w-7xl md:items-center md:flex">
