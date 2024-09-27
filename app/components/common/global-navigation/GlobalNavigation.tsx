5f7d566a3602875b707b42405f22591de73a900a
Mares Popa <popa.mares@gmail.com>
2024-09-27 16:39:15 +0300

    chore: discard unused forms

--- app/components/common/global-navigation/GlobalNavigation.tsx
+++ app/components/common/global-navigation/GlobalNavigation.tsx
@@ -12,11 +12,6 @@ const GlobalNavigation = () => {
   const [isNavOpen, setIsNavOpen] = useState(false)
   const hamburgerClosed = <Icon type="close" size={18} />
   const hamburgerOpen = <Icon type="hamburger" size={18} />
-  const { asPath } = useRouter()
-  const showOnlyCTA = asPath.includes('funnel') || asPath.includes('review')
-  const formLink = asPath.includes('funnel')
-    ? '/funnel#contact'
-    : '/review#reviewForm'
 
   return (
     <header className="bg-fuchsia-900 md:bg-fuchsia-900">
@@ -25,42 +20,29 @@ const GlobalNavigation = () => {
           <div className="mx-auto lg:max-w-7xl md:items-center md:flex md:justify-between ">
             <div className="flex items-center justify-between py-3 md:py-5 md:w-full">
               <GlobalLogo />
-              {!showOnlyCTA && (
-                <div className="md:hidden">
-                  <button
-                    className="p-2 text-white rounded-md outline-none focus:border-white focus:border"
-                    onClick={() => setIsNavOpen(!isNavOpen)}
-                  >
-                    {isNavOpen ? hamburgerOpen : hamburgerClosed}
-                  </button>
-                </div>
-              )}
-
-              {showOnlyCTA && (
-                <Link href={formLink} className={linkButtonStyle}>
-                  Get in touch
-                </Link>
-              )}
-            </div>
-            {!showOnlyCTA && (
-              <div>
-                <div
-                  className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
-                    isNavOpen ? 'block' : 'hidden'
-                  }`}
+              <div className="md:hidden">
+                <button
+                  className="p-2 text-white rounded-md outline-none focus:border-white focus:border"
+                  onClick={() => setIsNavOpen(!isNavOpen)}
                 >
-                  <GlobalLinks />
-                </div>
+                  {isNavOpen ? hamburgerOpen : hamburgerClosed}
+                </button>
               </div>
-            )}
+            </div>
+            <div>
+              <div
+                className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+                  isNavOpen ? 'block' : 'hidden'
+                }`}
+              >
+                <GlobalLinks />
+              </div>
+            </div>
           </div>
         </Container>
       </nav>
     </header>
   )
 }
-
-const linkButtonStyle =
-  'ml-auto flex items-center text-sm font-medium text-center capitalize hover:transition-all bg-yellow-300 hover:bg-yellow-400 focus:bg-yellow-400 text-gray-900 font-bold py-2 px-4 rounded duration-150 ease-in-out'
 
 export default GlobalNavigation
